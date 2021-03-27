1d24137cb12beb2e073abc84e94ce491cdedee22
Wasim <wchoudhari99@gmail.com>
2021-03-27 03:00:35 -0700

    fixed the code

--- wasim.py
+++ wasim.py
@@ -1,3 +1,4 @@
+
 import os
 List1 = [8, 9, 3, 6, 1, 10]
 List1.reverse()
@@ -6,10 +7,6 @@ print("The reverse list is",List1)
 List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
 List2.sort()
 print("The sorted list is", List2)
-
-List3 = []
-List3 = List1.copy()
-print("List3 +', List3)
 
 indexvalue = List2[2:6]
 print("The index values are", indexvalue)
